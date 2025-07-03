788d89f136ba79f538b5e29623ecb6c4538a186e
Armaldo <armando.v.mohr@gmail.com>
2025-07-02 20:01:30 -0400

    Ahora hay limite de caracteres y no se permiten simbolos

--- src/App.jsx
+++ src/App.jsx
@@ -10,38 +10,54 @@ function App() {
   });
 
   // Estado para el item que se está editando
-  const [itemToEdit, setItemToEdit] = useState(null);
+  const [itemToEdit, setItemToEdit] = useState();
 
   // Guarda en Local Storage cada vez que items cambia
   useEffect(() => {
     localStorage.setItem('items', JSON.stringify(items));
   }, [items]);
 
-  // Agrega o actualiza un item
-  const addOrUpdateItem = (value) => {
-    if (itemToEdit) {
-      // Modo editar
-      setItems(items.map(item => 
-        item.id === itemToEdit.id ? { ...item, value } : item
-      ));
-      setItemToEdit(null);
-    } else {
-      // Modo agregar
-      const newItem = { id: Date.now(), value };
-      setItems([...items, newItem]);
-    }
-  };
+ const addOrUpdateItem = (value) => {
+  // Validación 1: que tenga al menos 3 caracteres,value.trim elimina espacios sobrantes
+  if (value.trim().length < 3 || value.trim().length > 15) {
+    alert("El texto debe tener entre 3 y 15 caracteres.");
+    return;
+}
+  //La variable que se crea aca hace que el formulario no se le puedan incluir simbolos y solo admita lo que dentro
+  //el +$
+  const validFormat = /^[a-zA-Z0-9 ]+$/;
+  if (!validFormat.test(value)) {
+    alert("El texto solo puede contener letras, números y espacios.");
+    return;
+  }
+
+  if (itemToEdit) {
+    // Modo editar
+    setItems(items.map(item => 
+      item.id === itemToEdit.id ? { ...item, value } : item
+    ));
+    setItemToEdit(null);
+  } else {
+    // Modo agregar
+    const newItem = { id: Date.now(), value };
+    setItems([...items, newItem]);
+  }
+};
 
   // Elimina un item
   const deleteItem = (id) => {
+  if (window.confirm("¿Estás seguro de que quieres eliminar este item?")) {
     setItems(items.filter(item => item.id !== id));
-  };
+  }
+};
+
+  
 
   // Prepara un item para ser editado
   const editItem = (item) => {
     setItemToEdit(item);
   };
-
+  //Formularios aca abajo + Titulo de la pagina
   return (
     <div style={{ padding: '20px' }}>
       <h1>CRUD con Local Storage</h1>
