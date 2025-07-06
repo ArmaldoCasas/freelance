902071a8dc3a12bb18279ea894250f39c0e5d473
robinson <robinson>
2025-07-06 18:48:43 -0400

    Este commit añade la Api dentro del projecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Cliente from './components/Cliente';
 import Tarea from './components/Tarea';
-
+import DolarAPI from './components/DolarApi';
 function App() {
   return (
     <div>
       <h1>Control de Clientes y Tareas</h1>
       <Cliente />
       <Tarea />
+      <DolarAPI/>
     </div>
   );
 }
