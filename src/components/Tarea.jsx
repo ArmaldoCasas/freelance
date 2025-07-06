6654e46781dbcee28133636015ca958d66742164
Matias <matias.nonque@inacapmail.cl>
2025-07-06 16:34:11 -0400

    cambiar estilos de tarea y clientes

--- src/components/Tarea.jsx
+++ src/components/Tarea.jsx
@@ -56,17 +56,7 @@ function Tarea() {
   };
 
   return (
- <div
-  style={{
-    border: '1px solid #ccc',
-    marginBottom: '20px',
-    padding: '20px',
-    borderRadius: '8px',
-    boxShadow: '0 2px 8px rgba(0, 0, 0, 0.1)', // sombra
-    maxWidth: '1600px', //lo alarga
-    margin: '0 auto 20px auto'    // lo centra
-  }}
->
+    <div style={{ border: '1px solid #ccc', padding: '10px' }}>
       <h2>Tareas</h2>
       <Form
         addOrUpdateItem={addOrUpdateTarea}
